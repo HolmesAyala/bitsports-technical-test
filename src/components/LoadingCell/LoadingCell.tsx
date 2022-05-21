5b33dca5d858fac4b90fa4f3deb46a762a2965ad
Holmes Yesid Ayala Yaguara <holmes.ayala@muncher.com.co>
2022-05-21 17:24:59 -0500

    Loading cell

---
+++ src/components/LoadingCell/LoadingCell.tsx
@@ -0,0 +1,12 @@
+import LoadingIndicator from '../LoadingIndicator';
+import * as styled from './styled';
+
+const LoadingCell = () => {
+	return (
+		<styled.LoadingCell>
+			<LoadingIndicator />
+		</styled.LoadingCell>
+	);
+};
+
+export default LoadingCell;
---
+++ src/components/LoadingCell/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LoadingCell = styled('li')`
+	padding: ${({ theme }) => theme.spacing(4)}px;
+	display: flex;
+	justify-content: center;
+`;
