f277ac8f2cfd5694e5297e5843ed336f91087a4d
Holmes Yesid Ayala Yaguara <holmes.ayala@muncher.com.co>
2022-05-21 14:33:09 -0500

    Update font format

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -9,14 +9,14 @@ export const FONT_FAMILY: string = `${MAIN_FONT}, sans-serif`;
 export const fontsDeclaration = css`
 	@font-face {
 		font-family: ${MAIN_FONT};
-		src: url('../assets/fonts/sf-pro-display-regular.ttf') format('truetype');
+		src: url('../assets/fonts/sf-pro-display-regular.ttf') format('ttf');
 		font-weight: 400;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: ${MAIN_FONT};
-		src: url('../assets/fonts/sf-pro-display-bold.ttf') format('truetype');
+		src: url('../assets/fonts/sf-pro-display-bold.ttf') format('ttf');
 		font-weight: 700;
 		font-style: normal;
 	}
