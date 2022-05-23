4bf519ec56f2d881cb03f5888d307a39f7ca7471
Holmes Yesid Ayala Yaguara <holmes.ayala@muncher.com.co>
2022-05-23 16:27:11 -0500

    DataCell tests

---
+++ src/components/DataCell/DataCell.test.tsx
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { DEFAULT_THEME } from '../../styles/theme';
+import DataCell from './DataCell';
+
+describe('DataCell', () => {
+	it('Should render property', () => {
+		const propertyValue: string = 'test property';
+
+		render(
+			<ThemeProvider theme={DEFAULT_THEME}>
+				<DataCell property={propertyValue} />
+			</ThemeProvider>
+		);
+
+		screen.getByText(propertyValue);
+	});
+
+	it('Should render value', () => {
+		const value: string = 'test value';
+
+		render(
+			<ThemeProvider theme={DEFAULT_THEME}>
+				<DataCell value={value} />
+			</ThemeProvider>
+		);
+
+		screen.getByText(value);
+	});
+});
